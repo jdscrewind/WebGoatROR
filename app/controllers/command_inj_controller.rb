727c021db19db823a9af23d3cf18eae6548ac0b6
jdscrewind <joao.coelho@rewind.io>
2022-06-30 14:39:13 +0100

    Removes unnecessary files

--- app/controllers/command_inj_controller.rb
+++ app/controllers/command_inj_controller.rb
@@ -2,21 +2,21 @@ class CommandInjController < ApplicationController
   def index
     @param_code_inj = params[:param_code_inj] || ''
     # Code Inj
-    eval(@param_code_inj)
+    eval(@param_code_inj)                                                     # CHECKED by brakeman
 
     @param_comm_inj = params[:param_comm_inj] || ''
     # Command Injection
-    system("echo 'SYS' && " + @param_comm_inj)
-    puts `echo 'BTs' && #{@param_comm_inj}` # (backticks contain os command)
-    exec("echo 'EXEC' && " + @param_comm_inj)
-    spawn("echo 'SPAWN' && " + @param_comm_inj)
+    system("echo 'SYS' && " + @param_comm_inj)                                # CHECKED by brakeman
+    puts `echo 'BTs' && #{@param_comm_inj}` # (backticks contain os command)  # CHECKED by brakeman
+    exec("echo 'EXEC' && " + @param_comm_inj)                                 # CHECKED by brakeman
+    spawn("echo 'SPAWN' && " + @param_comm_inj)                               # CHECKED by brakeman
     open("| echo 'OPEN' && " + @param_comm_inj)
-    Process.exec("echo 'PROC_EXEC' && " + @param_comm_inj)
-    Process.spawn("echo 'PROC_SPAWN' && " + @param_comm_inj)
+    Process.exec("echo 'PROC_EXEC' && " + @param_comm_inj)                    # CHECKED by brakeman           
+    Process.spawn("echo 'PROC_SPAWN' && " + @param_comm_inj)                  # CHECKED by brakeman
     IO.binread("| echo 'IO_BINREAD' && " + @param_comm_inj)
     IO.binwrite("| echo 'IO_BINWRITE' && " + @param_comm_inj, " && ")
     IO.foreach("| echo 'IO_FOREACH' && " + @param_comm_inj) {}
-    IO.popen("echo 'IO_POPEN' && " + @param_comm_inj)
+    IO.popen("echo 'IO_POPEN' && " + @param_comm_inj)                         # CHECKED by brakeman
     IO.read("| echo 'IO_READ' && " + @param_comm_inj)
     IO.readlines("| echo 'IO_READLINES' && " + @param_comm_inj)
     IO.write("| echo 'IO_WRITE' && " + @param_comm_inj, "foo")
--- app/helpers/sql_inj_helper.rb
+++
@@ -1,2 +0,0 @@
-module SqlInjHelper
-end
